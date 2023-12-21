48612427bb0fe973db108f4414ff6ac853d63b23
Sam Penny <33956017+sam1penny@users.noreply.github.com>
2023-12-21 19:34:44 +0000

    add testing script

---
+++ scripts/test_coverage.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+find . -name '*.coverage' | xargs rm -f
+dune runtest --instrument-with bisect_ppx --force
+bisect-ppx-report html
+bisect-ppx-report summary
+open _coverage/index.html
