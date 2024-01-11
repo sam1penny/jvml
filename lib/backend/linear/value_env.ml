00f07d9e615160c428f92d0d36bf7ab42d9f4464
Sam Penny <33956017+sam1penny@users.noreply.github.com>
2024-01-11 17:25:58 +0000

    refactor value_env.ml

--- lib/backend/linear/value_env.ml
+++ lib/backend/linear/value_env.ml
@@ -2,48 +2,33 @@ open Instruction
 open Typing.Typed_ast
 module StringMap = Map.Make (String)
 
-type env = {
-  local_vars : string StringMap.t;
-  instance_fields : (string * type_expr) StringMap.t;
-  static_fields : (string * string * type_expr) StringMap.t;
-}
+type source =
+  | Local_var of string
+  | Instance_field of string * type_expr
+  | Static_field of string * string * type_expr
 
-let empty =
-  {
-    local_vars = StringMap.empty;
-    instance_fields = StringMap.empty;
-    static_fields = StringMap.empty;
-  }
+type env = source StringMap.t
+
+let empty = StringMap.empty
 
 let stdlib =
-  {
-    local_vars = StringMap.empty;
-    instance_fields = StringMap.empty;
-    static_fields =
-      StringMap.singleton "print" ("Std", "print", TyFun (TyVar "'a", TyUnit));
-  }
+  StringMap.singleton "print"
+    (Static_field ("Std", "print", TyFun (TyVar "'a", TyUnit)))
 
-let add_local_var k v env =
-  { env with local_vars = StringMap.add k v env.local_vars }
+let add_local_var k v env = StringMap.add k (Local_var v) env
 
-let add_instance_field k v env =
-  { env with instance_fields = StringMap.add k v env.instance_fields }
+let add_instance_field k (f, ty) env =
+  StringMap.add k (Instance_field (f, ty)) env
 
-let add_static_field k v env =
-  { env with static_fields = StringMap.add k v env.static_fields }
+let add_static_field k (clazz, f, ty) env =
+  StringMap.add k (Static_field (clazz, f, ty)) env
 
 let lookup k env =
-  match StringMap.find_opt k env.local_vars with
-  | Some b -> [ LOAD_REF b ]
-  (* put 'this' on stack, then load field *)
-  | None -> (
-      match StringMap.find_opt k env.instance_fields with
-      | Some (f, ty) -> [ LOAD_REF "0"; LOAD_FIELD (f, ty) ]
-      | None -> (
-          match StringMap.find_opt k env.static_fields with
-          | Some (clazz, name, ty) -> [ LOAD_STATIC (clazz, name, ty) ]
-          | None ->
-              raise
-              @@ Failure
-                   "looked up unknown var/field/staticmethod when lowering to \
-                    linear_ir"))
+  match StringMap.find_opt k env with
+  | Some (Local_var v) -> [ LOAD_REF v ]
+  | Some (Instance_field (f, ty)) -> [ LOAD_REF "0"; LOAD_FIELD (f, ty) ]
+  | Some (Static_field (clazz, name, ty)) -> [ LOAD_STATIC (clazz, name, ty) ]
+  | None ->
+      raise
+      @@ Failure
+           "looked up unknown var/field/staticmethod when lowering to linear_ir"
